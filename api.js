990d98ff1c9807392911f89f3cc0d7314eaeca85
airspace-andy <andreas@airspace.com>
2023-02-08 13:55:02 +0000

    Automatic Commit Wed Feb  8 01:55:02 PM UTC 2023

--- api.js
+++ api.js
@@ -60,7 +60,7 @@ cron.schedule("*/5 * * * *", function() {
               function initMap() {
                   var map = new google.maps.Map(document.getElementById('map'), {
                   center: {lat: dataPoints[0]['latitude'], lng: dataPoints[0]['longitude']},
-                   zoom: 800
+                   zoom: 18
               });
               dataPoints.forEach((data, index) => {
 
